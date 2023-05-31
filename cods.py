d9d5124fd0e09337cd9091e41bc5c2651c3a29ba
Briar <briar.j.mills@gmail.com>
2023-05-31 17:11:27 -0400

    Add code

---
+++ cods.py
@@ -0,0 +1,93 @@
+import logging
+
+from hdx.data.hdxobject import HDXError
+from hdx.utilities.base_downloader import DownloadError
+
+
+class COD:
+    def __init__(self, downloader, dataset, country, ab_url, em_url, ps_url, errors):
+        self.dataset = dataset
+        self.iso = country["#country+code+v_iso3"]
+        self.country = country["#country+name+preferred"]
+        self.downloader = downloader
+        self.errors = errors
+        self.service_urls = {
+            "ab": ab_url,
+            "em": em_url,
+            "ps": ps_url,
+        }
+
+    def get_service_resources(self, cod_type):
+        resources = list()
+        url = self.service_urls.get(cod_type)
+
+        if cod_type in ["ab", "em"]:
+            try:
+                self.downloader.download(url + "?f=pjson")
+            except DownloadError:
+                self.errors.add(f"{self.iso}: Could not get data from {url}")
+
+            service_list = self.downloader.get_json()["services"]
+            for service in service_list:
+                resource = dict()
+                if service["name"].split("/")[1][:3].upper() != self.iso:
+                    continue
+
+                resource["url"] = url + "/" + service["name"].split("/")[1] + "/" + service["type"]
+                resource["name"] = f"{service['name']} ({service['type']})"
+                resource["format"] = "Geoservice"
+
+                try:
+                    self.downloader.download(resource["url"] + "?f=pjson")
+                except DownloadError:
+                    self.errors.add(f"{self.iso}: could not get data from {resource['download_url']}")
+                    continue
+
+                resource["description"] = self.downloader.get_json()["serviceDescription"]
+
+                resources.append(resource)
+
+        if cod_type == "ps":
+            do_not_continue = False
+            for adm in range(0, 5):
+                resource = dict()
+                if do_not_continue:
+                    continue
+
+                resource["url"] = url.replace("/iso", f"/{self.iso}").replace("/adm/", f"/{adm}/")
+                resource["name"] = f"{self.iso.upper()} admin {adm} population"
+                resource["format"] = "JSON"
+
+                try:
+                    self.downloader.download(resource["url"])
+                except DownloadError:
+                    do_not_continue = True
+                    self.errors.add(f"{self.iso}: Could not get data from {url}")
+                    continue
+
+                resource["description"] = f"{self.country} administrative level " \
+                                          f"{adm} {self.downloader.get_json()['Year']} population statistics"
+
+                resources.append(resource)
+
+        return resources
+
+    def remove_service_resources(self):
+        updated = False
+        for resource in self.dataset.get_resources():
+            if resource.get_file_type().lower() not in ["geoservice", "json"] or "itos.uga.edu" not in resource["url"]:
+                continue
+
+            try:
+                self.dataset.delete_resource(resource, delete=False)
+            except HDXError:
+                self.errors.add(f"{self.dataset['name']}: Could not delete service resource")
+                continue
+
+            updated = True
+
+        return updated
+
+    def add_service_resources(self, resources):
+        self.dataset.add_update_resources(resources)
+        return
---
+++ run.py
@@ -0,0 +1,70 @@
+import logging
+from os.path import join, expanduser
+
+from cods import COD
+
+from hdx.api.configuration import Configuration
+from hdx.location.country import Country
+from hdx.data.hdxobject import HDXError
+from hdx.data.dataset import Dataset
+from hdx.facades.keyword_arguments import facade
+from hdx.utilities.downloader import Download
+from hdx.utilities.errors_onexit import ErrorsOnExit
+
+logger = logging.getLogger(__name__)
+
+lookup = "hdx-scraper-cods"
+
+
+def main(**ignore):
+    configuration = Configuration.read()
+
+    with ErrorsOnExit() as errors:
+        with Download(rate_limit={"calls": 1, "period": 0.1}) as downloader:
+            countries = Country.countriesdata()["countries"]
+            dataset_types = ["ab", "em", "ps"]
+
+            for country in countries:
+                for dataset_type in dataset_types:
+                    dataset = Dataset.read_from_hdx(f"cod-{dataset_type}-{country.lower()}")
+                    if not dataset:
+                        continue
+
+                    logger.info(f"Starting to update {country} {dataset_type.upper()} dataset")
+                    cod = COD(
+                        downloader,
+                        dataset,
+                        countries[country],
+                        configuration["ab_url"],
+                        configuration["em_url"],
+                        configuration["ps_url"],
+                        errors,
+                    )
+                    service_resources = cod.get_service_resources(dataset_type)
+                    updated = cod.remove_service_resources()
+
+                    if len(service_resources) == 0 and not updated:
+                        continue
+
+                    if len(service_resources) > 1:
+                        cod.add_service_resources(service_resources)
+
+                    try:
+                        dataset.update_in_hdx(
+                            hxl_update=False,
+                            batch_mode="KEEP_OLD",
+                            updated_by_script="HDX Scraper: CODS",
+                            ignore_fields=["num_of_rows", "resource:description"],
+                        )
+                    except HDXError as ex:
+                        errors.add(f"{dataset['name']}: {ex}")
+
+
+if __name__ == "__main__":
+    facade(
+        main,
+        hdx_site="stage",
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yml"),
+        user_agent_lookup=lookup,
+        project_config_yaml=join("config", "project_configuration.yml"),
+    )
